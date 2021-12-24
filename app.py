2cb4de44c8ced001f0f4aec07f7650b275cffd7e
ULSSY <96038813+ULSSY@users.noreply.github.com>
2021-12-24 11:46:32 +0900

    2021-12-24

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from eda_app import run_eda_app
+from ml_app import run_ml_app
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -6,22 +8,22 @@ import pickle
 
 from eda_app import run_eda_app
 from ml_app import run_ml_app
-# 자동으로 비주얼 코드가 임포트 해준다
+
 def main():
-    st.title('교통사고 현황 확인하기')
+    st.title('자동차 가격 예측 앱')
 
-    #사이드바 메뉴
-    menu=['Home','데이터검색','데이터분석']
-    choice=st.sidebar.selectbox('메뉴',menu)
+    # 사이드바 메뉴
+    menu = ['Home', 'EDA', 'ML']
+    choice = st.sidebar.selectbox('메뉴', menu)
 
-    if choice=='Home':
-        
-        st.write('교통사고 현황을 확인하실 수 있습니다.')
-        st.write('왼쪽의 사이드바에서 선택하세요')
-
-    elif choice =='데이터검색' :
+    if choice == 'Home' :
+        st.write('이 앱은 고객데이터와 자동차 구매 데이터에 대한 내용입니다. 해당 고객의 정보를 입력하면, 얼마정도의 차량을 구매할 수 있는지 예측해 줍니다.')
+        st.write('왼쪽의 사이드바에서 선택하세요.')
+    elif choice == 'EDA' :
         run_eda_app()
-    elif choice == '데이터분석':
+    elif choice == 'ML' :
         run_ml_app()
-if __name__ =='__main__':
+
+
+if __name__ == '__main__' :
     main()
